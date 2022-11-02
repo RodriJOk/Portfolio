1f2029f3fad0fa3ddbe111741806e2a195d149f3
RodriJOk <juarezrodrigo59@gmail.com>
2022-11-02 13:14:05 -0300

    Modifications in the styles and adjustments of article names

--- javascript.js
+++ javascript.js
@@ -167,7 +167,7 @@ const sr = ScrollReveal({
 })
 
 /*SCROLL HOME*/
-sr.reveal('.home__title', {})
+// sr.reveal('.home__title', {})
 sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
